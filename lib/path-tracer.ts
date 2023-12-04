ceebe04c7982fa42064d02bed5b9c00aecf6c31c
alpcihan <alpcihan.ac@gmail.com>
2023-12-04 12:00:47 +0100

    add camera uniforms

--- lib/path-tracer.ts
+++ lib/path-tracer.ts
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import screen_shader from './shaders/screen_shader.wgsl';
 import pathtracer_compute from './shaders/pathtracer_compute.wgsl';
 
@@ -17,7 +18,9 @@ class PathTracer {
         this._initPipelines();
     }
 
-    public render() {
+    public render(camera: THREE.PerspectiveCamera) {
+        this._updateUniforms(camera);
+
         const cmd: GPUCommandEncoder = this.m_device.createCommandEncoder();
 
         // path trace pass
@@ -47,6 +50,7 @@ class PathTracer {
 
             renderpass.end();
         }
+
         this.m_device.queue.submit([cmd.finish()]);
     }
 
@@ -67,6 +71,9 @@ class PathTracer {
     private m_screenPipelineBindGroup: GPUBindGroup;
 
     // assets
+    private m_uniformBuffer: GPUBuffer;
+    private m_uniformCPU: Float32Array; // do not change it (TODO: create a separate class for uniforms and make it readonly)
+
     private m_colorTexture: GPUTexture;
     private m_colorBufferView: GPUTextureView;
     private m_sampler: GPUSampler;
@@ -78,10 +85,10 @@ class PathTracer {
         const adapter = await gpu.requestAdapter() as GPUAdapter;
         if (!adapter) throw new Error('Adapter is not found on this browser.');
 
-        if(this.m_useReadWriteStorageExperimental) {
+        if (this.m_useReadWriteStorageExperimental) {
             const feature: GPUFeatureName = "chromium-experimental-read-write-storage-texture" as GPUFeatureName;
             if (!adapter.features.has(feature)) throw new Error("Read-write storage texture support is not available");
-            this.m_device = await adapter.requestDevice({requiredFeatures: [feature]}) as GPUDevice;
+            this.m_device = await adapter.requestDevice({ requiredFeatures: [feature] }) as GPUDevice;
             return;
         }
 
@@ -97,27 +104,41 @@ class PathTracer {
     }
 
     private _initAssets() {
-        this.m_colorTexture = this.m_device.createTexture({
-            size: {
-                width: this.m_canvas.width,
-                height: this.m_canvas.height,
-            },
-            format: "rgba8unorm",
-            usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.TEXTURE_BINDING
+        // uniform buffer
+        {
+            const uniformBufferSize = 
+                4 * 4 * 4 + // view
+                4 * 4 * 4   // projection
+            ;
+
+            this.m_uniformCPU = new Float32Array(uniformBufferSize / 4);
+
+            this.m_uniformBuffer = this.m_device.createBuffer({
+                size: uniformBufferSize,
+                usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+            });
         }
-        );
-        this.m_colorBufferView = this.m_colorTexture.createView();
 
-        const samplerDescriptor: GPUSamplerDescriptor = {
-            addressModeU: "repeat",
-            addressModeV: "repeat",
-            magFilter: "linear",
-            minFilter: "nearest",
-            mipmapFilter: "nearest",
-            maxAnisotropy: 1
-        };
-
-        this.m_sampler = this.m_device.createSampler(samplerDescriptor);
+        // color buffer + texture
+        {
+            this.m_colorTexture = this.m_device.createTexture({
+                size: {
+                    width: this.m_canvas.width,
+                    height: this.m_canvas.height,
+                },
+                format: "rgba8unorm",
+                usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.TEXTURE_BINDING
+            });
+            this.m_colorBufferView = this.m_colorTexture.createView();
+            this.m_sampler = this.m_device.createSampler({
+                addressModeU: "repeat",
+                addressModeV: "repeat",
+                magFilter: "linear",
+                minFilter: "nearest",
+                mipmapFilter: "nearest",
+                maxAnisotropy: 1
+            });
+        }
     }
 
     private _initPipelines() {
@@ -127,6 +148,11 @@ class PathTracer {
                 entries: [
                     {
                         binding: 0,
+                        visibility: GPUShaderStage.COMPUTE,
+                        buffer: {}
+                    },
+                    {
+                        binding: 1,
                         visibility: GPUShaderStage.COMPUTE,
                         storageTexture: {
                             access: "write-only",
@@ -142,6 +168,10 @@ class PathTracer {
                 entries: [
                     {
                         binding: 0,
+                        resource: { buffer: this.m_uniformBuffer }
+                    },
+                    {
+                        binding: 1,
                         resource: this.m_colorBufferView
                     }
                 ]
@@ -192,7 +222,7 @@ class PathTracer {
                         resource: this.m_colorBufferView
                     }
                 ]
-            }); 
+            });
 
             const screenPipelineLayout = this.m_device.createPipelineLayout({
                 bindGroupLayouts: [screenBindGroupLayout]
@@ -225,6 +255,13 @@ class PathTracer {
                 }
             });
         }
+    }
+
+    private _updateUniforms(camera: THREE.PerspectiveCamera) {
+        this.m_uniformCPU.set(camera.modelViewMatrix.invert().toArray(), 0);
+        this.m_uniformCPU.set(camera.projectionMatrix.invert().toArray(), 4*4);
+
+        this.m_device.queue.writeBuffer(this.m_uniformBuffer, 0, this.m_uniformCPU);
     }
 };
 
--- lib/shaders/pathtracer_compute.wgsl
+++ lib/shaders/pathtracer_compute.wgsl
@@ -1,22 +1,28 @@
 // enable chromium_experimental_read_write_storage_texture;
 
-@group(0) @binding(0) var colorBuffer: texture_storage_2d<rgba8unorm, write>;
+@group(0) @binding(0) var<uniform> uniforms: Uniforms;
+@group(0) @binding(1) var colorBuffer: texture_storage_2d<rgba8unorm, write>;
 
 struct Sphere {
-    center: vec3<f32>,
+    center: vec3f,
     radius: f32,
-}
+};
 
 struct Ray {
-    direction: vec3<f32>,
-    origin: vec3<f32>,
-}
+    direction: vec3f,
+    origin: vec3f,
+};
+
+struct Uniforms {
+    view_i: mat4x4f,
+    projection_i: mat4x4f,
+};
 
 @compute @workgroup_size(8,8,1)
-fn main(@builtin(global_invocation_id) GlobalInvocationID : vec3<u32>) {
+fn main(@builtin(global_invocation_id) globalInvocationID : vec3u) {
 
-    let screenSize: vec2<i32> = vec2<i32>(textureDimensions(colorBuffer));
-    let texelCoord : vec2<i32> = vec2<i32>(i32(GlobalInvocationID.x), i32(GlobalInvocationID.y));
+    let screenSize: vec2i = vec2i(textureDimensions(colorBuffer));
+    let texelCoord : vec2i = vec2i(i32(globalInvocationID.x), i32(globalInvocationID.y));
 
     if (texelCoord.x >= screenSize.x || texelCoord.y >= screenSize.y) {
         return;
@@ -24,25 +30,25 @@ fn main(@builtin(global_invocation_id) GlobalInvocationID : vec3<u32>) {
 
     let horizontal: f32 = (f32(texelCoord.x) - f32(screenSize.x) / 2) / f32(screenSize.x); // [-1, 1]
     let vertical: f32 = (f32(texelCoord.y) - f32(screenSize.y) / 2) / f32(screenSize.x);   // [-1, 1]
-    let forward: vec3<f32> = vec3<f32>(1.0, 0.0, 0.0);
-    let right: vec3<f32> = vec3<f32>(0.0, -1.0, 0.0);
-    let up: vec3<f32> = vec3<f32>(0.0, 0.0, 1.0);
+    let forward: vec3f = vec3f(1.0, 0.0, 0.0);
+    let right: vec3f = vec3f(0.0, -1.0, 0.0);
+    let up: vec3f = vec3f(0.0, 0.0, 1.0);
 
-    var mySphere: Sphere;
-    mySphere.center = vec3<f32>(3.0, 0.0, 0.0);
-    mySphere.radius = 0.5;
+    var sphere: Sphere;
+    sphere.center = vec3f(3.0, 0.0, 0.0);
+    sphere.radius = 0.5;
 
-    var myRay: Ray;
-    myRay.direction = normalize(forward + horizontal * right + vertical * up);
-    myRay.origin = vec3<f32>(0.0, 0.0, 0.0);
+    var ray: Ray;
+    ray.direction = normalize(forward + horizontal * right + vertical * up);
+    ray.origin = origin;
 
-    var pixel_color : vec3<f32> = vec3<f32>(0.0, 0.0, 0.0);
+    var pixel_color : vec3f = vec3f(1.0, 0.0, 0.0);
 
-    if (hit(myRay, mySphere)) {
-        pixel_color = vec3<f32>(1.0, 1.0, 1.0);
+    if (hit(ray, sphere)) {
+        pixel_color = vec3f(0.0, 1.0, 0.0);
     }
 
-    textureStore(colorBuffer, texelCoord, vec4<f32>(pixel_color, 1.0));
+    textureStore(colorBuffer, texelCoord, vec4f(pixel_color, 1.0));
 }
 
 fn hit(ray: Ray, sphere: Sphere) -> bool {
@@ -53,5 +59,4 @@ fn hit(ray: Ray, sphere: Sphere) -> bool {
     let discriminant: f32 = b * b - 4.0 * a * c;
 
     return discriminant > 0;
-    
 }
