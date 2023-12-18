0d8cf3d5f51a9b7e1fe76fb9ca9d4fa93bbac0c6
alpcihan <alpcihan.ac@gmail.com>
2023-12-18 23:23:38 +0100

    add fps displayer

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import * as wt from '../lib/wtrace';
 
 var canvas: HTMLCanvasElement;
+var fpsElement: HTMLTextAreaElement;
+var fpsDisplayTimer: number;
 var camera: THREE.PerspectiveCamera;
 var cameraController: wt.CameraController;
 var pathTracer: wt.PathTracer;
@@ -12,15 +14,26 @@ const onUpdate = () => {
 
 	// render
 	pathTracer.render(camera);
+
+	if(fpsDisplayTimer > 0.1) {
+		fpsElement!.innerText = `FPS: ${Math.floor(1/wt.Application.getDeltaTime())}`;
+		fpsDisplayTimer = 0;
+	}
+	
+	fpsDisplayTimer += wt.Application.getDeltaTime();
 }
 
 const main = async () => {
 	await wt.Application.init();
 
-    canvas = document.getElementById('wt_canvas-webgpu') as HTMLCanvasElement;    
+    canvas = document.getElementById('wt_canvas-webgpu') as HTMLCanvasElement;  
+	pathTracer = new wt.PathTracer(canvas);
+
     camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.01, 1000);
 	cameraController = new wt.CameraController({camera: camera});
-	pathTracer = new wt.PathTracer(canvas);
+	
+	fpsElement = document.getElementById('wt_fps') as HTMLTextAreaElement;
+	fpsDisplayTimer = 0;
 
 	wt.Application.run(onUpdate);
 }
