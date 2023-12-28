fef5df9d14d0c50ab9b57c84f0d49c70a3843478
alpcihan <alpcihan.ac@gmail.com>
2023-12-28 14:30:58 +0100

    fix camera controller rotation

--- lib/camera-controller.ts
+++ lib/camera-controller.ts
@@ -1,74 +1,131 @@
 import * as THREE from "three";
 import { Key } from "./input-system-types";
 import { InputSystem } from "./input-system";
+import { clamp } from "three/src/math/MathUtils";
 
 interface CameraControllerProps {
-  camera: THREE.PerspectiveCamera;
+	camera: THREE.PerspectiveCamera;
+}
+
+interface CameraControllerInputs {
+	forwardInput: Key;
+	backwardInput: Key;
+	rightInput: Key;
+	leftInput: Key;
+	upInput: Key;
+	downInput: Key;
+	pitchInput: Key;
+	yawnInput: Key;
+	pitchCounterClockwiseInput: Key;
+	yawnCounterClockwiseInput: Key;
 }
 
 class CameraController {
-  constructor(props: CameraControllerProps) {
-    this.m_camera = props.camera;
-  }
+	constructor(props: CameraControllerProps) {
+		this.m_camera = props.camera;
 
-  public forwardInput: Key = Key.KeyW;
-  public backwardInput: Key = Key.KeyS;
-  public rightInput: Key = Key.KeyD;
-  public leftInput: Key = Key.KeyA;
-  public upInput: Key = Key.KeyShift;
-  public downInput: Key = Key.KeySpace;
-  public pitchInput: Key = Key.KeyArrowUp;
-  public yawnInput: Key = Key.KeyArrowRight;
-  public pitchCounterClockwiseInput: Key = Key.KeyArrowDown;
-  public yawnCounterClockwiseInput: Key = Key.KeyArrowLeft;
+		// TODO: read rest from the camera
+		this.m_euler = new THREE.Euler(0,0,0); 
+		this.m_front = new THREE.Vector3(0,0,-1);
+		this.m_up = new THREE.Vector3(0,1,0);
+	}
 
-  public speed: number = 1;
-  public rotationSpeed: number = 30;
+	public inputs: CameraControllerInputs = {
+		forwardInput: Key.KeyW,
+		backwardInput: Key.KeyS,
+		rightInput: Key.KeyD,
+		leftInput: Key.KeyA,
+		upInput: Key.KeyShift,
+		downInput: Key.KeySpace,
+		pitchInput: Key.KeyArrowUp,
+		yawnInput: Key.KeyArrowRight,
+		pitchCounterClockwiseInput: Key.KeyArrowDown,
+		yawnCounterClockwiseInput: Key.KeyArrowLeft,
+	};
 
-  public update(deltaTime: number) {
-    this.m_isUpdated = false;
+	public speed: number = 1;
+	public rotationSpeed: number = 90;
 
-    // update the camera vectors
-    const deltaSpeed = this.speed * deltaTime;
-    const deltaRotateSpeed = this.rotationSpeed * deltaTime;
+	public update(deltaTime: number) {
+		this.m_isUpdated = false;
 
-    if (InputSystem.isKeyPressed(this.forwardInput)) {
-      this.m_camera.translateZ(deltaSpeed);
-      this.m_isUpdated = true;
-    }
-    if (InputSystem.isKeyPressed(this.backwardInput)) {
-      this.m_camera.translateZ(-deltaSpeed);
-      this.m_isUpdated = true;
-    }
-    if (InputSystem.isKeyPressed(this.upInput)) {
-      this.m_camera.translateY(deltaSpeed);
-      this.m_isUpdated = true;
-    }
-    if (InputSystem.isKeyPressed(this.downInput)) {
-      this.m_camera.translateY(-deltaSpeed);
-      this.m_isUpdated = true;
-    }
-    if (InputSystem.isKeyPressed(this.rightInput)) {
-      this.m_camera.translateX(-deltaSpeed);
-      this.m_isUpdated = true;
-    }
-    if (InputSystem.isKeyPressed(this.leftInput)) {
-      this.m_camera.translateX(deltaSpeed);
-      this.m_isUpdated = true;
-    }
+		this.m_isUpdated = this._processInputs(deltaTime);
+		
+		if (!this.m_isUpdated) return;
 
-    if (!this.m_isUpdated) return;
+		this.m_camera.updateMatrixWorld(true);
+	}
 
-    this.m_camera.updateMatrix();
-    this.m_camera.updateMatrixWorld(true);
-  }
+	public isUpdated(): boolean {
+		return this.m_isUpdated;
+	}
+	
+	private m_camera: THREE.PerspectiveCamera;
+	private m_isUpdated: boolean;
+	private m_euler: THREE.Euler;
+	private m_front: THREE.Vector3;
+	private m_up: THREE.Vector3;
 
-  public isUpdated(): boolean {
-    return this.m_isUpdated;
-  }
+	private _processInputs(deltaTime: number): boolean {
+		const deltaSpeed = this.speed * deltaTime;
+    	const deltaRotationSpeed = this.rotationSpeed * deltaTime * THREE.MathUtils.DEG2RAD;
 
-  private m_camera: THREE.PerspectiveCamera;
-  private m_isUpdated: boolean;
+		let isUpdated = false;
+
+		// rotation inputs
+		if (InputSystem.isKeyPressed(this.inputs.yawnInput)) {
+			this.m_euler.y -= deltaRotationSpeed;
+			isUpdated = true;
+		}
+    	if (InputSystem.isKeyPressed(this.inputs.yawnCounterClockwiseInput)) {
+			this.m_euler.y += deltaRotationSpeed;
+			isUpdated = true;
+		}
+		if (InputSystem.isKeyPressed(this.inputs.pitchInput)) {
+			this.m_euler.x += deltaRotationSpeed;
+			isUpdated = true;
+		}
+    	if (InputSystem.isKeyPressed(this.inputs.pitchCounterClockwiseInput)) {
+			this.m_euler.x -= deltaRotationSpeed;
+			isUpdated = true;
+		}
+		
+		let q: THREE.Quaternion = new THREE.Quaternion();
+		q.setFromEuler(this.m_euler);
+
+		const lookAt: THREE.Vector3 = this.m_front.clone().applyQuaternion(q);
+		const right: THREE.Vector3 = this.m_front.clone().cross(this.m_up);
+
+		// translation inputs
+		if (InputSystem.isKeyPressed(this.inputs.forwardInput)) {
+			this.m_camera.position.add(lookAt.clone().multiplyScalar(deltaSpeed));
+			isUpdated = true;
+		}
+		if (InputSystem.isKeyPressed(this.inputs.backwardInput)) {
+			this.m_camera.position.add(lookAt.clone().multiplyScalar(-deltaSpeed));
+			isUpdated = true;
+		}
+		if (InputSystem.isKeyPressed(this.inputs.rightInput)) {
+			this.m_camera.position.add(right.clone().multiplyScalar(deltaSpeed));
+			isUpdated = true;
+		}
+		if (InputSystem.isKeyPressed(this.inputs.leftInput)) {
+			this.m_camera.position.add(right.clone().multiplyScalar(-deltaSpeed));
+			isUpdated = true;
+		}
+		if (InputSystem.isKeyPressed(this.inputs.upInput)) {
+			this.m_camera.position.add(this.m_up.clone().multiplyScalar(deltaSpeed));
+			isUpdated = true;
+		}
+		if (InputSystem.isKeyPressed(this.inputs.downInput)) {
+			this.m_camera.position.add(this.m_up.clone().multiplyScalar(-deltaSpeed));
+			isUpdated = true;
+		}
+
+		this.m_camera.lookAt(this.m_camera.position.clone().add(lookAt));
+
+		return isUpdated;
+	}
 }
 
 export { CameraController };
