b7eb340bb189800b3689f0ce950fdda05a70d7e0
Umut Ercan <umut.ercan@tum.de>
2024-01-15 16:28:59 +0100

    Add BVH class

--- lib/core/scene/scene-data-manager.ts
+++ lib/core/scene/scene-data-manager.ts
@@ -1,5 +1,6 @@
 import { MeshModel } from "../objects/model/mesh-model";
 import { IGPU } from "../renderer/igpu";
+import { BVH } from "./bvh";
 
 class SceneDataManager {
     public constructor() {
@@ -18,6 +19,9 @@ class SceneDataManager {
 
     private m_vertices: Float32Array;
     private m_vertexBuffer: GPUBuffer;
+    private m_bvhNodes: BVH;
+    private m_bvhNodeBuffer: GPUBuffer;
+    private m_triangleIdxBuffer: GPUBuffer;
 
     private _updateVertexBuffer(): void {
         this.m_vertexBuffer = IGPU.get().createBuffer({
@@ -26,6 +30,21 @@ class SceneDataManager {
         });
 
         IGPU.get().queue.writeBuffer(this.m_vertexBuffer, 0, this.m_vertices);
+    }
+    private _updateBVH(): void {
+        // const bvh = new BVH();
+        // bvh.build(this.m_vertices);
+        // this.m_bvhNodes = bvh.nodes;
+        // this._updateBVHBuffers();
+    }
+    
+    private _updateBVHBuffers(): void {
+        // this.m_bvhNodeBuffer = IGPU.get().createBuffer({
+        //     size: this.m_bvhNodes.byteLength,
+        //     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+        // });
+
+        // IGPU.get().queue.writeBuffer(this.m_bvhNodeBuffer, 0, this.m_bvhNodes);
     }
 }
 
