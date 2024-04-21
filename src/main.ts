0c06bd0471d0bf6ca965c86c0f5d2a235d01b37d
umut ercan <umuterc123@gmail.com>
2024-04-21 16:50:00 +0200

    change scene to default

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ const main = async () => {
     // init the application
     await wt.Application.init(canvas);
 
-    let scene: wt.Scene = await test.createGLTFDamagedHelmet();
+    let scene: wt.Scene = await test.createMeetManScene();
     
     // load the scene
     wt.SceneManager.loadScene(scene);
